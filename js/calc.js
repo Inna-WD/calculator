11a2744dacf89216b818fd7c224342c0a04e94bf
Inna-WD <inna.druker@gmail.com>
2019-12-04 11:12:22 -0500

    412

--- js/calc.js
+++ js/calc.js
@@ -1,4 +1,8 @@
-//fetch buttons
+
+
+
+
+//pants buttons
 
 let pantsMinusButton = document.getElementById("lessPants");
 let pantsPlusButton = document.getElementById("morePants");
@@ -11,7 +15,14 @@ label.innerHTML=parseInt(label.innerHTML)-1
 function pantsPlus(){
     let label=document.getElementById("pantsQty");
     label.innerHTML=parseInt(label.innerHTML)+1
-    }
+}
+
+
+function simpleCalc(){
+let pantsTotal=label.innerHTML*40.99;
+console.log(label.innerHTML)
+}
 
 pantsMinusButton.addEventListener('click',pantsMinus);
 pantsPlusButton.addEventListener('click',pantsPlus);
+calculate.addEventListener(`click`,simpleCalc);
