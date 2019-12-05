81ee6962fc4c695316a982970cc416d619dcfc01
Inna-WD <inna.druker@gmail.com>
2019-12-05 09:10:40 -0500

    0512

--- js/calc.js
+++ js/calc.js
@@ -47,16 +47,27 @@ label.innerHTML=parseInt(label.innerHTML)+1
 // calculation
 function simpleCalc(){
 let labelP=document.getElementById("pantsQty");
-let qtyP=document.getElementById("pantsTotal");
+// let qtyP=document.getElementById("pantsTotal");
 let labelS=document.getElementById("shirtQty");
-let qtyS=document.getElementById("shirtTotal");
+// let qtyS=document.getElementById("shirtTotal");
+// let labelT=document.getElementById("subTotal");
+// let labelH=document.getElementById("tax");
+// let labelTT=document.getElementById("total");
 
 var x=parseInt(labelP.innerHTML)*40.99;
 pantsTotal.innerHTML=x.toFixed(2);
 
 var y=parseInt(labelS.innerHTML)*10.99;
-shirtTotal.innerHTML=y.toFixed(2)
+shirtTotal.innerHTML=y.toFixed(2);
 
+var z=x+y;
+subTotal.innerHTML=z.toFixed(2);
+
+var t=z*.13;
+tax.innerHTML=t.toFixed(2)
+
+var q=t+z;
+total.innerHTML=q.toFixed(2)
 }
 
 pantsMinusButton.addEventListener('click',pantsMinus);
