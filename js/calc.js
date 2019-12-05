15b9400f024d01cbe0372c8344d4f19dee06523b
Inna-WD <inna.druker@gmail.com>
2019-12-04 22:28:17 -0500

    0412

--- js/calc.js
+++ js/calc.js
@@ -2,7 +2,7 @@
 
 
 
-//pants buttons
+//Pants
 
 let pantsMinusButton = document.getElementById("lessPants");
 let pantsPlusButton = document.getElementById("morePants");
@@ -10,19 +10,57 @@ let pantsPlusButton = document.getElementById("morePants");
 function pantsMinus(){
 let label=document.getElementById("pantsQty");
 label.innerHTML=parseInt(label.innerHTML)-1
+if (label.innerHTML<0){
+    label.innerHTML=0
+}
 }
 
 function pantsPlus(){
-    let label=document.getElementById("pantsQty");
-    label.innerHTML=parseInt(label.innerHTML)+1
+let label=document.getElementById("pantsQty");
+label.innerHTML=parseInt(label.innerHTML)+1
 }
 
 
+//Shirts
+
+let shirtMinusButton = document.getElementById("lessShirt");
+let shirtPlusButton = document.getElementById("moreShirt");
+
+function shirtMinus(){
+let label=document.getElementById("shirtQty");
+
+label.innerHTML=parseInt(label.innerHTML)-1
+if (label.innerHTML<0){
+    label.innerHTML=0
+    // console.log(label.innerHTML)
+}
+}
+
+
+
+function shirtPlus(){
+let label=document.getElementById("shirtQty");
+label.innerHTML=parseInt(label.innerHTML)+1
+}
+
+
+// calculation
 function simpleCalc(){
-let pantsTotal=label.innerHTML*40.99;
-console.log(label.innerHTML)
+let labelP=document.getElementById("pantsQty");
+let qtyP=document.getElementById("pantsTotal");
+let labelS=document.getElementById("shirtQty");
+let qtyS=document.getElementById("shirtTotal");
+
+var x=parseInt(labelP.innerHTML)*40.99;
+pantsTotal.innerHTML=x.toFixed(2);
+
+var y=parseInt(labelS.innerHTML)*10.99;
+shirtTotal.innerHTML=y.toFixed(2)
+
 }
 
 pantsMinusButton.addEventListener('click',pantsMinus);
 pantsPlusButton.addEventListener('click',pantsPlus);
+shirtMinusButton.addEventListener('click',shirtMinus);
+shirtPlusButton.addEventListener('click',shirtPlus);
 calculate.addEventListener(`click`,simpleCalc);
