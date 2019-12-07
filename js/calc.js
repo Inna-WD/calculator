971723c273cbc549fc9f38b3d9659d7de0fa71eb
Inna-WD <inna.druker@gmail.com>
2019-12-07 14:45:34 -0500

    0712

--- js/calc.js
+++ js/calc.js
@@ -3,10 +3,10 @@
 
 
 //Pants
-
 let pantsMinusButton = document.getElementById("lessPants");
 let pantsPlusButton = document.getElementById("morePants");
 
+// function that decreases the number of pants by 1 and turns the value to 0 if it becames negative
 function pantsMinus() {
     let label = document.getElementById("pantsQty");
     label.innerHTML = parseInt(label.innerHTML) - 1
@@ -14,7 +14,7 @@ function pantsMinus() {
         label.innerHTML = 0
     }
 }
-
+// function that increases the number of pants by 1
 function pantsPlus() {
     let label = document.getElementById("pantsQty");
     label.innerHTML = parseInt(label.innerHTML) + 1
@@ -25,17 +25,16 @@ function pantsPlus() {
 
 let shirtMinusButton = document.getElementById("lessShirt");
 let shirtPlusButton = document.getElementById("moreShirt");
-
+// function that decreases the number of shirts by 1 and turns the value to 0 if it becames negative
 function shirtMinus() {
     let label = document.getElementById("shirtQty");
-
     label.innerHTML = parseInt(label.innerHTML) - 1
     if (label.innerHTML < 0) {
         label.innerHTML = 0
         // console.log(label.innerHTML)
     }
 }
-
+// function that increases the number of shirts by 1
 function shirtPlus() {
     let label = document.getElementById("shirtQty");
     label.innerHTML = parseInt(label.innerHTML) + 1
@@ -45,12 +44,7 @@ function shirtPlus() {
 // calculation of total and taxes
 function simpleCalc() {
     let labelP = document.getElementById("pantsQty");
-    // let qtyP=document.getElementById("pantsTotal");
     let labelS = document.getElementById("shirtQty");
-    // let qtyS=document.getElementById("shirtTotal");
-    // let labelT=document.getElementById("subTotal");
-    // let labelH=document.getElementById("tax");
-    // let labelTT=document.getElementById("total");
 
     var x = parseInt(labelP.innerHTML) * 40.99;
     pantsTotal.innerHTML = x.toFixed(2);
@@ -70,34 +64,34 @@ function simpleCalc() {
 
 
 // coupons
-
 function couponFunction() {
     var txt;
     var t;
     var coupon = prompt("Enter coupon code here:", "NOTAX or FIFTYFIFTY");
+    // empty coupon value will not change the total
     if (coupon == null || coupon == "") {
         txt = "I understand you don't have a coupon code ...";
     }
+    // no tax coupon will calculate the total without 13% tax\
     else if (coupon == "NOTAX") {
         txt = subTotal.innerHTML;
         txt = "Your final price for today is: $ " + txt;
     }
-
+    // the total will be divided by 2 and them 13% tax will be added  
     else if (coupon == "FIFTYFIFTY") {
-        t = subTotal.innerHTML / 2;
-        txt = t * 1.13;
+        t = (subTotal.innerHTML / 2)*1.13;
+        txt = t.toFixed(2);
         txt = "Your final price for today is: $ " + txt;
     }
-
+    // any othe value will not change the total 
         else {
         txt = "This coupon code doesn't exist, please enter the valid coupon";
     }
-
        document.getElementById("result").innerHTML = txt;
 
 }
 
-
+// adding listeners to all 6 bottins in the page  
 pantsMinusButton.addEventListener('click', pantsMinus);
 pantsPlusButton.addEventListener('click', pantsPlus);
 shirtMinusButton.addEventListener('click', shirtMinus);
